c4de9d4a770801227b72f0aa8151c353aef975e6
Daniel <119364597+dmaniel1@users.noreply.github.com>
2025-02-22 09:58:56 -0600

    Add files via upload

---
+++ shapes/sketch.js
@@ -0,0 +1,15 @@
+function setup() {
+  createCanvas(400, 400);
+}
+
+function draw() {
+  background(220);
+  fill("red");
+  rect(200, 200, 22, 20);
+  fill("blue");
+  circle(220, 220, 20);
+  fill("purple");
+  triangle(215, 240, 235, 240, 225, 220);
+}
+const color = "gray";
+console.log(color);
